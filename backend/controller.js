8da096bcd02e23e038b5f15ce33353924de3e8b5
Divya Kelaskar <divyakelaskar@gmail.com>
2025-09-23 18:30:54 +0530

    increase access token validity

--- backend/controller.js
+++ backend/controller.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 // Helpers
 const createTokens = (userId) => {
   const accessToken = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: '15m',
+    expiresIn: '1d',
   });
   const refreshToken = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
     expiresIn: '7d',
